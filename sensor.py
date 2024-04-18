e9a3d455d2e907cd6907b4d327c1f6d586dedeac
maalekj <maalekj@gmail.com>
2024-04-18 05:29:56 +0300

    change the vital signals types from float to int, the sensor now send all the vitals based on random

--- sensor.py
+++ sensor.py
@@ -1,8 +1,11 @@
-from time import sleep
+import random
+import time
 import rticonnextdds_connector as rti
 from os import path as os_path
 
 file_path = os_path.dirname(os_path.abspath(__file__))
+
+patient_id = 1  # TODO: Change this to the patient ID you got as parameter from the command line argument (like python sensor.py <patient_id>)
 
 with rti.open_connector(
     config_name="MyDomainParticipantLibrary::SensorDomainParticipant",
@@ -11,7 +14,11 @@ with rti.open_connector(
     output = connector.get_output("VitalPublisher::SensorToServerTopicWriter")
 
     for i in range(1, 500):
-        output.instance.set_number("heartRate", 55)
+        output.instance.set_number("patient_ID", patient_id)
+        output.instance.set_number("heartRate", random.randint(40, 160))
+        output.instance.set_number("bloodPressure", random.randint(60, 180))
+        output.instance.set_number("oxygenSaturation", random.randint(60, 100))
+        output.instance.set_number("timestamp", time.time())
         output.write()
         print("Sent")
-        sleep(5)
+        time.sleep(5)
